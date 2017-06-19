7ae07eb3439ccae47aadd87a91b1b4036f6aaf1e
aaksingh <deathakanote@gmail.com>
2017-06-19 07:27:08 -0700

    First Commuit

---
+++ app/src/main/java/com/example/ne/newmap/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ne.newmap;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
